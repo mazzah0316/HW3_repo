a4bae521cc0f4af1009908afa05ac4e934ee3b51
mazzah0316 <mazzah@oregonstate.edu>
2021-04-25 20:22:52 -0700

    Update hudson_mazza_hw1.py
    Added Error Handling

--- hudson_mazza_hw1.py
+++ hudson_mazza_hw1.py
@@ -1,8 +1,11 @@
-year = int(input("Enter Year: "))
-
-if year % 4 == 0 and year % 100 != 0:
-    print(year, "is a Leap Year")
-elif year % 400 ==0:
-    print(year, "is a Leap Year")
-else:
-    print(year, "is not a Leap Year")
+year = input("Enter Year: ")
+try:
+    year_int = int(year)
+    if year_int % 4 == 0 and year_int % 100 != 0:
+        print(year_int, "is a Leap Year")
+    elif year_int % 400 ==0:
+        print(year_int, "is a Leap Year")
+    else:
+        print(year_int, "is not a Leap Year")
+except ValueError:
+    print ("Input is not a valid year")
